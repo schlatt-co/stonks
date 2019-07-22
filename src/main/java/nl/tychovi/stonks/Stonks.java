3ffe64ab42f1eb3fa9921253ccee8ca808c2f9f6
TychoVI <t.vanisterdael@gmail.com>
2019-07-22 15:57:06 +0200

    Initial commit

---
+++ src/main/java/nl/tychovi/stonks/Stonks.java
@@ -0,0 +1,15 @@
+package nl.tychovi.stonks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Stonks extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
