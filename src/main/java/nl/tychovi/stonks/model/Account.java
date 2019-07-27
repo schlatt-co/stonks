8ed84c0d49cc6c644488f9b94c31ede462567962
Charles Lockie <lockiecharles@gmail.com>
2019-07-27 16:35:01 +0100

    Filled out account classes

--- src/main/java/nl/tychovi/stonks/model/Account.java
+++ src/main/java/nl/tychovi/stonks/model/Account.java
@@ -1,12 +1,15 @@
 package nl.tychovi.stonks.model;
 
-public class Account {
+public abstract class Account {
+    public Account(String name) {
+        this.name = name;
+    }
     String name;
 
-    public String GetName() {
+    public String getName() {
         return name;
     }
-    public boolean SetName(String name) {
+    public boolean setName(String name) {
         //Don't allow blank names
         if (name.isBlank()) {
             return false;
@@ -14,4 +17,13 @@ public class Account {
         this.name = name;
         return true;
     }
+
+    //Add money to the account
+    public abstract void pay(String playerUUID, double amount);
+    //Withdraw money from the account (to pay someone / another company)
+    public abstract boolean withdraw(String playerUUID, double amount);
+    //See the total quantity of money in the account
+    public abstract double getBalance();
+
+
 }
--- src/main/java/nl/tychovi/stonks/model/HoldingsAccount.java
+++ src/main/java/nl/tychovi/stonks/model/HoldingsAccount.java
@@ -1,4 +1,62 @@
 package nl.tychovi.stonks.model;
 
-public class HoldingsAccount {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class HoldingsAccount extends Account {
+
+    private List<Holding> holdings = new ArrayList<Holding>();
+
+    public HoldingsAccount(String name, Holding firstHolding) {
+        super(name);
+        holdings.add(firstHolding);
+    }
+
+    private double getTotalShare() {
+        double total = 0;
+        for (Holding h : holdings) {
+            total += h.getShare();
+        }
+        return total;
+    }
+
+    //Returns the holding for the player entered, if none is found then return nothing
+    private Optional<Holding> getPlayerHolding(String player) {
+        for (Holding h: holdings){
+            if (h.player.equals(player)) {
+                return Optional.of(h);
+            }
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public void pay(String playerUUID, double amount) {
+        //Pay a fraction of the amount into each holding proportional to its share
+        for (Holding h : holdings) {
+            //Multiply the amount by the fractional share
+            h.payIn(amount * (h.getShare() / getTotalShare()));
+        }
+    }
+
+    @Override
+    public boolean withdraw(String playerUUID, double amount) {
+        //It is not possible to withdraw from a holdings account
+        return false;
+    }
+
+    @Override
+    public double getBalance() {
+        double total = 0;
+        for (Holding h : holdings) {
+            total += h.getBalance();
+        }
+        return total;
+    }
+
+    public boolean AddHolding(String player, double share) {
+        Holding h;
+        return false;
+    }
 }
