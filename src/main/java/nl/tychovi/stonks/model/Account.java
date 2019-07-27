307cdeb03ced6ccd052e23660b7865b93654f5ab
Charles Lockie <lockiecharles@gmail.com>
2019-07-27 15:54:14 +0100

    Setup second branch

--- src/main/java/nl/tychovi/stonks/model/Account.java
+++ src/main/java/nl/tychovi/stonks/model/Account.java
@@ -1,4 +1,17 @@
 package nl.tychovi.stonks.model;
 
 public class Account {
+    String name;
+
+    public String GetName() {
+        return name;
+    }
+    public boolean SetName(String name) {
+        //Don't allow blank names
+        if (name.isBlank()) {
+            return false;
+        }
+        this.name = name;
+        return true;
+    }
 }
