53e3d89692fc2c8e08e89afb1039982561bf1f78
TychoVI <t.vanisterdael@gmail.com>
2019-07-28 23:20:15 +0200

    Revert "Filled out account classes"
    This reverts commit 8ed84c0d49cc6c644488f9b94c31ede462567962.

--- src/main/java/nl/tychovi/stonks/model/Account.java
+++ src/main/java/nl/tychovi/stonks/model/Account.java
@@ -1,15 +1,12 @@
 package nl.tychovi.stonks.model;
 
-public abstract class Account {
-    public Account(String name) {
-        this.name = name;
-    }
+public class Account {
     String name;
 
-    public String getName() {
+    public String GetName() {
         return name;
     }
-    public boolean setName(String name) {
+    public boolean SetName(String name) {
         //Don't allow blank names
         if (name.isBlank()) {
             return false;
@@ -17,13 +14,4 @@ public abstract class Account {
         this.name = name;
         return true;
     }
-
-    //Add money to the account
-    public abstract void pay(String playerUUID, double amount);
-    //Withdraw money from the account (to pay someone / another company)
-    public abstract boolean withdraw(String playerUUID, double amount);
-    //See the total quantity of money in the account
-    public abstract double getBalance();
-
-
 }
