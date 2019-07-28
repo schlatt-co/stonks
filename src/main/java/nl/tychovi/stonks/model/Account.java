6c3085df6661119d0baaa33d80dc7256962a86a5
TychoVI <t.vanisterdael@gmail.com>
2019-07-28 23:20:26 +0200

    Revert "Setup second branch"
    This reverts commit 307cdeb03ced6ccd052e23660b7865b93654f5ab.

--- src/main/java/nl/tychovi/stonks/model/Account.java
+++ src/main/java/nl/tychovi/stonks/model/Account.java
@@ -1,17 +1,4 @@
 package nl.tychovi.stonks.model;
 
 public class Account {
-    String name;
-
-    public String GetName() {
-        return name;
-    }
-    public boolean SetName(String name) {
-        //Don't allow blank names
-        if (name.isBlank()) {
-            return false;
-        }
-        this.name = name;
-        return true;
-    }
 }
