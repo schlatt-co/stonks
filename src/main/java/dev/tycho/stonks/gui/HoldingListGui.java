4132bb277eaf751e3d801e1f616156b03c943161
Charles Lockie <lockiecharles@gmail.com>
2019-09-02 13:55:25 +0100

    Left click to withdraw holding

--- src/main/java/dev/tycho/stonks/gui/HoldingListGui.java
+++ src/main/java/dev/tycho/stonks/gui/HoldingListGui.java
@@ -17,76 +17,98 @@ import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HoldingListGui implements InventoryProvider {
 
-    public static DatabaseManager databaseManager;
-    public static InventoryManager inventoryManager;
+  public static DatabaseManager databaseManager;
+  public static InventoryManager inventoryManager;
 
-    private HoldingsAccount holdingsAccount;
+  private HoldingsAccount holdingsAccount;
 
-    public HoldingListGui(HoldingsAccount holdingsAccount) {
-        this.holdingsAccount = holdingsAccount;
-    }
+  public HoldingListGui(HoldingsAccount holdingsAccount) {
+    this.holdingsAccount = holdingsAccount;
+  }
 
-    public static SmartInventory getInventory(HoldingsAccount holdingsAccount) {
-        return SmartInventory.builder()
-                .id("holdingList")
-                .provider(new HoldingListGui(holdingsAccount))
-                .manager(inventoryManager)
-                .size(5, 9)
-                .title(holdingsAccount.getName() +" info")
-                .build();
-    }
+  public static SmartInventory getInventory(HoldingsAccount holdingsAccount) {
+    return SmartInventory.builder()
+        .id("holdingList")
+        .provider(new HoldingListGui(holdingsAccount))
+        .manager(inventoryManager)
+        .size(5, 9)
+        .title(holdingsAccount.getName() + " info")
+        .build();
+  }
 
-    @Override
-    public void init(Player player, InventoryContents contents) {
-        AccountLink link = databaseManager.getAccountLinkDao().getAccountLink(holdingsAccount);
+  @Override
+  public void init(Player player, InventoryContents contents) {
+    AccountLink link = databaseManager.getAccountLinkDao().getAccountLink(holdingsAccount);
 
-        contents.fillRow(0, ClickableItem.empty(Util.item(Material.BLACK_STAINED_GLASS_PANE, " ")));
-        contents.fillRow(4, ClickableItem.empty(Util.item(Material.BLACK_STAINED_GLASS_PANE, " ")));
-        contents.set(0,0, ClickableItem.of(Util.item(Material.BARRIER, "Back to info"), e -> player.performCommand("stonks accounts " + link.getCompany().getName())));
+    contents.fillRow(0, ClickableItem.empty(Util.item(Material.BLACK_STAINED_GLASS_PANE, " ")));
+    contents.fillRow(4, ClickableItem.empty(Util.item(Material.BLACK_STAINED_GLASS_PANE, " ")));
+    contents.set(0, 0, ClickableItem.of(Util.item(Material.BARRIER, "Back to info"), e -> player.performCommand("stonks accounts " + link.getCompany().getName())));
 
-        contents.set(0, 4, ClickableItem.empty(Util.item(Material.GOLD_INGOT, holdingsAccount.getName())));
+    contents.set(0, 4, ClickableItem.empty(Util.item(Material.GOLD_INGOT, holdingsAccount.getName())));
 
 
-        ForeignCollection<Holding> holdings = holdingsAccount.getHoldings();
+    ForeignCollection<Holding> holdings = holdingsAccount.getHoldings();
 
-        Pagination pagination = contents.pagination();
+    Pagination pagination = contents.pagination();
 
-        ClickableItem[] items = new ClickableItem[holdings.size()];
+    ClickableItem[] items = new ClickableItem[holdings.size()];
 
-        int i = 0;
-        for(Holding holding : holdings) {
-            OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(holding.getPlayer());
-            ClickableItem item = ClickableItem.of(Util.playerHead(offlinePlayer.getName(), offlinePlayer,
-                    ChatColor.WHITE + "Balance: " + ChatColor.GREEN + "£" + holding.getBalance(),
-                    ChatColor.WHITE + "Share: " + ChatColor.YELLOW + holding.getShare(),
-                    ChatColor.RED + "Right click to remove holding."), e -> {
-                if(e.getClick().isRightClick()) {
-                    player.performCommand("stonks removeholding " + link.getId() + " " + offlinePlayer.getName());
-                }
-            });
-            items[i] = item;
-            i++;
+    int i = 0;
+    for (Holding holding : holdings) {
+      OfflinePlayer offlinePlayer = Bukkit.getOfflinePlayer(holding.getPlayer());
+
+      List<String> lore = new ArrayList<>();
+      lore.add(ChatColor.WHITE + "Balance: " + ChatColor.GREEN + "£" + holding.getBalance());
+      lore.add(ChatColor.WHITE + "Share: " + ChatColor.YELLOW + holding.getShare());
+      if (player.getUniqueId().equals(holding.getPlayer()))
+        lore.add(ChatColor.GREEN + "Left click to withdraw your holding.");
+      lore.add(ChatColor.RED + "Right click to delete holding.");
+
+
+      ClickableItem item = ClickableItem.of(Util.playerHead(offlinePlayer.getName(), offlinePlayer,
+          lore), e -> {
+        if (e.getClick().isRightClick()) {
+          player.performCommand("stonks removeholding " + link.getId() + " " + offlinePlayer.getName());
+        } else if (e.getClick().isLeftClick() && player.getUniqueId().equals(holding.getPlayer())) {
+          player.performCommand("stonks withdraw " + holding.getBalance() + " " + link.getId());
         }
-
-        pagination.setItems(items);
-        pagination.setItemsPerPage(27);
-
-        pagination.addToIterator(contents.newIterator(SlotIterator.Type.HORIZONTAL, 1, 0));
-
-
-        contents.set(4, 3, ClickableItem.of(Util.item(Material.ARROW, "Previous page"),
-                e -> getInventory(holdingsAccount).open(player, pagination.previous().getPage())));
-        contents.set(4, 5, ClickableItem.of(Util.item(Material.ARROW, "Next page"),
-                e -> getInventory(holdingsAccount).open(player, pagination.next().getPage())));
-
+      });
+      items[i] = item;
+      i++;
     }
 
-    @Override
-    public void update(Player player, InventoryContents contents) {
+    pagination.setItems(items);
+    pagination.setItemsPerPage(27);
 
-    }
+    pagination.addToIterator(contents.newIterator(SlotIterator.Type.HORIZONTAL, 1, 0));
+
+
+    contents.set(4, 3, ClickableItem.of(Util.item(Material.ARROW, "Previous page"),
+        e ->
+
+            getInventory(holdingsAccount).
+
+                open(player, pagination.previous().
+
+                    getPage())));
+    contents.set(4, 5, ClickableItem.of(Util.item(Material.ARROW, "Next page"),
+        e ->
+
+            getInventory(holdingsAccount).
+
+                open(player, pagination.next().
+
+                    getPage())));
+
+  }
+
+  @Override
+  public void update(Player player, InventoryContents contents) {
+
+  }
 }
