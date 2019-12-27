067d2aecdfecbad0b839ae127c0a6e9c0bcfca2b
JRoy <joshroy126@gmail.com>
2019-12-26 22:32:13 -0500

    Make chat command use computeIfAbsent instead of dropping message

--- src/main/java/dev/tycho/stonks/command/ChatCommand.java
+++ src/main/java/dev/tycho/stonks/command/ChatCommand.java
@@ -3,15 +3,14 @@ package dev.tycho.stonks.command;
 import dev.tycho.stonks.gui.CompanySelectorGui;
 import dev.tycho.stonks.managers.PlayerStateData;
 import dev.tycho.stonks.managers.Repo;
-import dev.tycho.stonks.model.core.Company;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.Collection;
 import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ChatCommand implements CommandExecutor {
 
@@ -19,23 +18,30 @@ public class ChatCommand implements CommandExecutor {
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     Player player = (Player) sender;
     HashMap<Player, Integer> chatSelectionStore = PlayerStateData.getInstance().getChatSelectionStore();
-    if (args.length == 0 || !chatSelectionStore.containsKey(player)) {
-      Collection<Company> list = Repo.getInstance().companiesWhereMember(player);
+    Integer companyPk = PlayerStateData.getInstance().getChatSelectionStore().computeIfAbsent(player, p -> {
+      AtomicInteger selected = new AtomicInteger(-1);
       new CompanySelectorGui.Builder()
-          .companies(list)
+          .companies(Repo.getInstance().companiesWhereMember(player))
           .title("Select company for chat.")
           .companySelected((company -> {
-            chatSelectionStore.put(player, company.pk);
+            selected.set(company.pk);
             player.sendMessage(ChatColor.DARK_GREEN + "Stonks> " + ChatColor.GREEN + "Set " + company.name + " as company chat channel. You may send a message now using /cc <message>");
           }))
           .show(player);
-    } else {
-      StringBuilder message = new StringBuilder(player.getDisplayName() + ": ");
-      for (String arg : args) {
-        message.append(arg).append(" ");
-      }
-      Repo.getInstance().sendMessageToAllOnlineMembers(Repo.getInstance().companies().get(chatSelectionStore.get(player)), message.toString());
+      return selected.get();
+    });
+
+    if (companyPk == -1) {
+      player.sendMessage(ChatColor.DARK_GREEN + "Stonks> " + ChatColor.GREEN + "You failed to select a company!");
+      return true;
     }
+
+    StringBuilder message = new StringBuilder(player.getDisplayName() + ": ");
+    for (String arg : args) {
+      message.append(arg).append(" ");
+    }
+    Repo.getInstance().sendMessageToAllOnlineMembers(Repo.getInstance().companies().get(chatSelectionStore.get(player)), message.toString());
+
     return true;
   }
 }
