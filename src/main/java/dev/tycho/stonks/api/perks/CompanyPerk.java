6779150eb61a9758ac6450a660608038e8030b2c
JRoy <joshroy126@gmail.com>
2020-01-08 18:37:12 -0500

    Finalize some CompanyPerk methods

--- src/main/java/dev/tycho/stonks/api/perks/CompanyPerk.java
+++ src/main/java/dev/tycho/stonks/api/perks/CompanyPerk.java
@@ -3,20 +3,23 @@ package dev.tycho.stonks.api.perks;
 import dev.tycho.stonks.model.core.Company;
 import dev.tycho.stonks.model.core.Member;
 import org.bukkit.Material;
+import org.bukkit.plugin.Plugin;
 
 public abstract class CompanyPerk {
 
+  private final Plugin plugin;
   private final String name;
   private final Material icon;
   private final int price;
   private final String[] description;
   private final CompanyPerkAction[] perkActions;
 
-  public CompanyPerk(String name, Material icon, int price, String... description) {
-    this(name, icon, price, description, new CompanyPerkAction[]{});
+  public CompanyPerk(Plugin plugin, String name, Material icon, int price, String... description) {
+    this(plugin, name, icon, price, description, new CompanyPerkAction[]{});
   }
 
-  public CompanyPerk(String name, Material icon, int price, String[] description, CompanyPerkAction... companyPerkActions) {
+  public CompanyPerk(Plugin plugin, String name, Material icon, int price, String[] description, CompanyPerkAction... companyPerkActions) {
+    this.plugin = plugin;
     this.name = name;
     this.icon = icon;
     this.price = price;
@@ -26,23 +29,27 @@ public abstract class CompanyPerk {
 
   public abstract void onPurchase(Company company, Member purchaser);
 
-  public String getName() {
+  public Plugin getPlugin() {
+    return plugin;
+  }
+
+  public final String getName() {
     return name;
   }
 
-  public Material getIcon() {
+  public final Material getIcon() {
     return icon;
   }
 
-  public int getPrice() {
+  public final int getPrice() {
     return price;
   }
 
-  public String[] getDescription() {
+  public final String[] getDescription() {
     return description;
   }
 
-  public CompanyPerkAction[] getPerkActions() {
+  public final CompanyPerkAction[] getPerkActions() {
     return perkActions;
   }
 }
