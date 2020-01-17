5115636713083e9f020904b8da6ea344bfaed7a0
JRoy <joshroy126@gmail.com>
2020-01-17 18:56:19 -0500

    Inline TransferCommandSub

--- src/main/java/dev/tycho/stonks/command/subs/account/TransferCommandSub.java
+++ src/main/java/dev/tycho/stonks/command/subs/account/TransferCommandSub.java
@@ -40,85 +40,82 @@ public class TransferCommandSub extends ModularCommandSub {
     List<Company> list = Repo.getInstance().companies().getAll();
     new CompanySelectorGui.Builder()
         .companies(list)
-        .title("Select a company to transfer from")
+        .title("Select company to transfer from")
         .companySelected((company -> {
           //Cache the next screen
           new AccountSelectorGui.Builder()
               .company(company)
-              .title("Select which account to transfer from")
-              .accountSelected(transferFrom -> payCompany(player, transferFrom, message, amount))
+              .title("Select account to transfer from")
+              .accountSelected(transferFrom -> new CompanySelectorGui.Builder()
+                  .companies(list)
+                  .title("Select a company to transfer to")
+                  .companySelected((companyTo -> {
+                    //Cache the next screen
+                    new AccountSelectorGui.Builder()
+                        .company(companyTo)
+                        .title("Select which account to transfer to")
+                        .accountSelected(transferTo -> payCompany(player, transferFrom, transferTo, companyTo, message, amount))
+                        .show(player);
+                  }))
+                  .show(player))
               .show(player);
         }))
         .show(player);
   }
 
-  private void payCompany(Player sender, Account transferFrom, String message, double amount) {
-    List<Company> list = Repo.getInstance().companies().getAll();
-    new CompanySelectorGui.Builder()
-        .companies(list)
-        .title("Select a company to transfer to")
-        .companySelected((company -> {
-          //Cache the next screen
-          new AccountSelectorGui.Builder()
-              .company(company)
-              .title("Select which account to transfer to")
-              .accountSelected(transferTo -> {
-                if (amount < 0) {
-                  sendMessage(sender, "You cannot pay a negative number");
-                  return;
-                }
+  private void payCompany(Player sender, Account transferFrom, Account transferTo, Company companyTo, String message, double amount) {
+    if (amount < 0) {
+      sendMessage(sender, "You cannot pay a negative number");
+      return;
+    }
 
-                if (transferFrom.getTotalBalance() < amount) {
-                  sendMessage(sender, "Insufficient funds!");
-                  return;
-                }
+    if (transferFrom.getTotalBalance() < amount) {
+      sendMessage(sender, "Insufficient funds!");
+      return;
+    }
 
-                IAccountVisitor visitor = new IAccountVisitor() {
-                  @Override
-                  public void visit(CompanyAccount a) {
-                    //With a company account we need to verify they have withdraw permission
-                    if (!Repo.getInstance().companies().get(transferFrom.companyPk).getMember(sender).hasManagamentPermission()) {
-                      sendMessage(sender, "You have insufficient permissions to withdraw money from this account!");
-                      return;
-                    }
-                    if (a.getTotalBalance() < amount) {
-                      sendMessage(sender, "That account doesn't have enough funds to complete this transaction!");
-                      return;
-                    }
+    IAccountVisitor visitor = new IAccountVisitor() {
+      @Override
+      public void visit(CompanyAccount a) {
+        //With a company account we need to verify they have withdraw permission
+        if (!Repo.getInstance().companies().get(transferFrom.companyPk).getMember(sender).hasManagamentPermission()) {
+          sendMessage(sender, "You have insufficient permissions to withdraw money from this account!");
+          return;
+        }
+        if (a.getTotalBalance() < amount) {
+          sendMessage(sender, "That account doesn't have enough funds to complete this transaction!");
+          return;
+        }
 
-                    Repo.getInstance().withdrawFromAccount(sender.getUniqueId(), a, amount, message);
-                  }
+        Repo.getInstance().withdrawFromAccount(sender.getUniqueId(), a, amount, message);
+      }
 
-                  @Override
-                  public void visit(HoldingsAccount a) {
-                    //Check to see if they own a holding in this holdingsaccount
-                    Holding h = a.getPlayerHolding(sender.getUniqueId());
-                    if (h == null) {
-                      sendMessage(sender, "You do not have a holding in this account!");
-                      return;
-                    }
+      @Override
+      public void visit(HoldingsAccount a) {
+        //Check to see if they own a holding in this holdingsaccount
+        Holding h = a.getPlayerHolding(sender.getUniqueId());
+        if (h == null) {
+          sendMessage(sender, "You do not have a holding in this account!");
+          return;
+        }
 
-                    if (h.balance < amount) {
-                      sendMessage(sender, "That holding doesn't have enough funds to complete this transaction!");
-                      return;
-                    }
+        if (h.balance < amount) {
+          sendMessage(sender, "That holding doesn't have enough funds to complete this transaction!");
+          return;
+        }
 
-                    Repo.getInstance().withdrawFromHolding(sender.getUniqueId(), h, amount, message);
-                  }
-                };
-                transferFrom.accept(visitor);
+        Repo.getInstance().withdrawFromHolding(sender.getUniqueId(), h, amount, message);
+      }
+    };
+    transferFrom.accept(visitor);
 
-                Repo.getInstance().payAccount(sender.getUniqueId(), message, transferTo, amount);
+    Repo.getInstance().payAccount(sender.getUniqueId(), message, transferTo, amount);
 
-                //Tell the user we paid the account
-                sendMessage(sender, "Paid " + ChatColor.YELLOW + company.name + " (" + transferTo.name + ")" + ChatColor.YELLOW + " $" + Util.commify(amount) + ChatColor.GREEN + "!");
+    //Tell the user we paid the account
+    sendMessage(sender, "Paid " + ChatColor.YELLOW + companyTo.name + " (" + transferTo.name + ")" + ChatColor.YELLOW + " $" + Util.commify(amount) + ChatColor.GREEN + "!");
 
-                //Send a message to all managers in the company that are online that the company got paid
-                Repo.getInstance().sendMessageToAllOnlineManagers(
-                    company, sender.getDisplayName() + ChatColor.GREEN + " paid " + ChatColor.YELLOW + " " + company.name + " (" + transferTo.name + ") $" + Util.commify(amount));
-              })
-              .show(sender);
-        }))
-        .show(sender);
+    //Send a message to all managers in the company that are online that the company got paid
+    Repo.getInstance().sendMessageToAllOnlineManagers(
+        companyTo, sender.getDisplayName() + ChatColor.GREEN + " paid " + ChatColor.YELLOW + " " + companyTo.name + " (" + transferTo.name + ") $" + Util.commify(amount));
   }
 }
