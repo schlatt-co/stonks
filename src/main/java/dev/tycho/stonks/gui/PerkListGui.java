57467cb980cbf09bb5d1c55cb216bdbfee225dab
JRoy <joshroy126@gmail.com>
2020-03-09 22:50:10 -0400

    Fix issues with perk purchases with missing Admin account

--- src/main/java/dev/tycho/stonks/gui/PerkListGui.java
+++ src/main/java/dev/tycho/stonks/gui/PerkListGui.java
@@ -63,7 +63,10 @@ public class PerkListGui extends CollectionGui<CompanyPerk> {
 
         Repo.getInstance().withdrawFromAccount(player.getUniqueId(), a, perk.getPrice());
         PerkManager.getInstance().awardPerk(company, perk, company.getMember(player));
-        Repo.getInstance().payAccount(player.getUniqueId(), "Perk Purchase for " + company.name, Objects.requireNonNull(StonksAPI.getCompany("Admins")).accounts.stream().filter(p -> p.name.equals("Main")).findFirst().get(), perk.getPrice());
+        if (StonksAPI.getCompany("Admins") != null) {
+          //noinspection OptionalGetWithoutIsPresent
+          Repo.getInstance().payAccount(player.getUniqueId(), "Perk Purchase for " + company.name, Objects.requireNonNull(StonksAPI.getCompany("Admins")).accounts.stream().filter(p -> p.name.equals("Main")).findFirst().get(), perk.getPrice());
+        }
         sendMessage(player, "Perk purchased successfully!");
         Repo.getInstance().sendMessageToAllOnlineMembers(company, player.getName() + " has purchased the " + perk.getName() + " perk for the company: " + company.name);
       }
