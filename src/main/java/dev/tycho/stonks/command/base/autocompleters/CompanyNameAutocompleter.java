2b034b3b26d49decb15fe8343a1659ec58b49dc7
Tsarcasm <lockiecharles@gmail.com>
2020-04-17 11:00:11 +0100

    Company name autocompletion no longer cares about character case

--- src/main/java/dev/tycho/stonks/command/base/autocompleters/CompanyNameAutocompleter.java
+++ src/main/java/dev/tycho/stonks/command/base/autocompleters/CompanyNameAutocompleter.java
@@ -12,9 +12,9 @@ public class CompanyNameAutocompleter extends ArgumentAutocompleter {
   @Override
   public List<String> getCompletions(Player player, String arg) {
     List<String> names = new ArrayList<>();
-    List<Company> companies = Repo.getInstance().companies().getAllWhere(c -> c.isMember(player) && (arg.isEmpty() || c.name.contains(arg)));
+    List<Company> companies = Repo.getInstance().companies().getAllWhere(c -> c.isMember(player) && (arg.isEmpty() || c.name.toUpperCase().contains(arg.toUpperCase())));
     // Limit the number of companies we return to 10
-    for (int i = 0; i < Math.min(10, companies.size()); i++) {
+    for (int i = 0; i < Math.min(20, companies.size()); i++) {
       names.add(companies.get(i).name);
     }
     return names;
