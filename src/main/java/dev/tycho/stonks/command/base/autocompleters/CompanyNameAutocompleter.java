ec50d69ae39b372e7adc404ab67c21e6c6979b60
Tsarcasm <lockiecharles@gmail.com>
2020-04-17 11:01:49 +0100

    Format

--- src/main/java/dev/tycho/stonks/command/base/autocompleters/CompanyNameAutocompleter.java
+++ src/main/java/dev/tycho/stonks/command/base/autocompleters/CompanyNameAutocompleter.java
@@ -12,7 +12,8 @@ public class CompanyNameAutocompleter extends ArgumentAutocompleter {
   @Override
   public List<String> getCompletions(Player player, String arg) {
     List<String> names = new ArrayList<>();
-    List<Company> companies = Repo.getInstance().companies().getAllWhere(c -> c.isMember(player) && (arg.isEmpty() || c.name.toUpperCase().contains(arg.toUpperCase())));
+    List<Company> companies = Repo.getInstance().companies().getAllWhere(
+        c -> c.isMember(player) && (arg.isEmpty() || c.name.toUpperCase().contains(arg.toUpperCase())));
     // Limit the number of companies we return to 10
     for (int i = 0; i < Math.min(20, companies.size()); i++) {
       names.add(companies.get(i).name);
