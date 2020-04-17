987d99eb75d10e3636a266ba62a8f7df1bb04b71
Tsarcasm <lockiecharles@gmail.com>
2020-04-17 16:52:43 +0100

    Fixed company name autocompletion to show all (not just member's) companies

--- src/main/java/dev/tycho/stonks/command/base/autocompleters/CompanyNameAutocompleter.java
+++ src/main/java/dev/tycho/stonks/command/base/autocompleters/CompanyNameAutocompleter.java
@@ -13,7 +13,7 @@ public class CompanyNameAutocompleter extends ArgumentAutocompleter {
   public List<String> getCompletions(Player player, String arg) {
     List<String> names = new ArrayList<>();
     List<Company> companies = Repo.getInstance().companies().getAllWhere(
-        c -> c.isMember(player) && (arg.isEmpty() || c.name.toUpperCase().contains(arg.toUpperCase())));
+        c -> (arg.isEmpty() || c.name.toUpperCase().contains(arg.toUpperCase())));
     // Limit the number of companies we return to 10
     for (int i = 0; i < Math.min(20, companies.size()); i++) {
       names.add(companies.get(i).name);
